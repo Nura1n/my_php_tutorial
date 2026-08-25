a47c5dff2afb04323988b3b0d3a0270c25755195
Nura1n <itsjustain.h@gmail.com>
2026-08-25 22:27:47 +0800

    tuto12 - continue & break

--- tuto/tuto12.php
+++ tuto/tuto12.php
@@ -4,51 +4,53 @@
 
 $price = 20;
 
-    //if($price < 10){
-        //echo 'the condition is met';
-    //} elseif ( $price < 30){
-        //echo 'elseif condition met';
-    //} else {
-        //echo 'the condition is not met';
-     //}
+//if($price < 10){
+//echo 'the condition is met';
+//} elseif ( $price < 30){
+//echo 'elseif condition met';
+//} else {
+//echo 'the condition is not met';
+//}
 
-   $products = [
-        ['name'=> 'shiny star','price' => 20],
-        ['name'=> 'green shell','price'=> 10],
-        ['name'=> 'red shell','price'=> 15],
-        ['name'=> 'gold coin','price'=> 5],
-        ['name'=> 'lightning bolt','price'=> 40],
-        ['name'=> 'banana skin','price'=> 2]
-    ];
+$products = [
+    ['name' => 'shiny star', 'price' => 20],
+    ['name' => 'green shell', 'price' => 10],
+    ['name' => 'red shell', 'price' => 15],
+    ['name' => 'gold coin', 'price' => 5],
+    ['name' => 'lightning bolt', 'price' => 40],
+    ['name' => 'banana skin', 'price' => 2]
+];
 
-    foreach ($products as $product){
-        if($product['name'] === 'lightning bolt'){
-            break;
-        }
-
-        if ($product['price'] > 15){
-            continue;
-        }
-
-        echo $product['name'] . '<br />';
-
+foreach ($products as $product) {
+    if ($product['name'] === 'lightning bolt') {
+        break;
     }
+
+    if ($product['price'] > 15) {
+        continue;
+    }
+
+    echo $product['name'] . '<br />';
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>PHP Tutorials</title>
 </head>
+
 <body>
 
 
 
 </body>
+
 </html>
 
-//nota 
+//nota
 
-break = break out of a loop no matter where we are 
+break = break out of a loop no matter where we are
 continue = stop the code at a specific place, go back up to the foreach and continue on the next product
