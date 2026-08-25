e3a5449e845b2cff148c952c98fdfe9da55cee8b
Nura1n <itsjustain.h@gmail.com>
2026-08-25 22:51:29 +0800

    footer for the pizza website

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,4 @@
-
-    <footer class = "sestion"> // materialize classes,gives space
-        <div class = "center grey-text">Copyright 2026 Ninja Pizza</div>
-    </footer>
+<footer class="sestion"> // materialize classes,gives space
+    <div class="center grey-text">Copyright 2026 Ninja Pizza</div>
+</footer>
 </body>
